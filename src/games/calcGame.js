45bb253d7292bec0ca2c72d18da600a78d638d4c
pollyleyka <polka321@rambler.ru>
2022-08-03 20:07:53 +0400

    fixed calc game

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -1,26 +1,36 @@
-import gamesLogic from '../index.js';
+import getGameLogic from '../index.js';
 import getRndInteger from '../helper.js';
 
 const gameRules = 'What is the result of the expression?';
 
-const getGameData = () => {
+const getExpression = () => {
   const operators = ['+', '-', '*'];
   const expression = `${getRndInteger(1, 10)} ${operators[getRndInteger(0, 3)]} ${getRndInteger(1, 10)}`;
+  return expression;
+};
 
-  const coll = expression.split(' ');
-  const a = Number(coll[0]);
-  const b = Number(coll[2]);
+const getExpectedAnswer = (expression) => {
   let expectedAnswer = '';
-  if (coll.includes('+')) {
+  const splitedExpressions = expression.split(' ');
+  const a = Number(splitedExpressions[0]);
+  const b = Number(splitedExpressions[2]);
+
+  if (splitedExpressions.includes('+')) {
     expectedAnswer += (a + b);
-  } else if (coll.includes('-')) {
+  } else if (splitedExpressions.includes('-')) {
     expectedAnswer += (a - b);
   } else {
     expectedAnswer += (a * b);
   }
+  return expectedAnswer;
+};
+
+const getGameData = () => {
+  const expression = getExpression();
+  const expectedAnswer = getExpectedAnswer(expression);
   return [expression, expectedAnswer];
 };
 
 export default () => {
-  gamesLogic(gameRules, getGameData);
+  getGameLogic(gameRules, getGameData);
 };
