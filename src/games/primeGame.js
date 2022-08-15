66e2bff393c92eb0afcf29e36db36f623e874afd
pollyleyka <polka321@rambler.ru>
2022-08-15 13:54:43 +0400

    fixed prime game

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -3,15 +3,34 @@ import getRndInteger from '../helper.js';
 
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+// const getGameData = () => {
+//   const expression = getRndInteger();
+//   let expectedAnswer;
+//   const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
+//     47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+//   if (primeNumbers.includes(expression)) {
+//     expectedAnswer = 'yes';
+//   } else {
+//     expectedAnswer = 'no';
+//   }
+//   return [expression, expectedAnswer];
+// };
+const isPrime = (num) => {
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const getGameData = () => {
   const expression = getRndInteger();
   let expectedAnswer;
-  const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
-    47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-  if (primeNumbers.includes(expression)) {
-    expectedAnswer = 'yes';
-  } else {
+  if (expression <= 1) {
     expectedAnswer = 'no';
+  } else {
+    expectedAnswer = isPrime(expression) ? 'yes' : 'no';
   }
   return [expression, expectedAnswer];
 };
