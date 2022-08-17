22e4fa2775bd94a020d20e330fe74be786355c8b
pollyleyka <polka321@rambler.ru>
2022-08-17 19:40:34 +0400

    fexed prime game function logic

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -4,6 +4,9 @@ import getRandomNumber from '../helper.js';
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return false;
@@ -14,12 +17,7 @@ const isPrime = (number) => {
 
 const getPrimeGameData = () => {
   const expression = getRandomNumber();
-  let expectedAnswer;
-  if (expression <= 1) {
-    expectedAnswer = 'no';
-  } else {
-    expectedAnswer = isPrime(expression) ? 'yes' : 'no';
-  }
+  const expectedAnswer = isPrime(expression) ? 'yes' : 'no';
   return [expression, expectedAnswer];
 };
 
