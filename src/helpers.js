dc1dd0b774a29c6e5d982ab1178b3db2167b991e
pollyleyka <polka321@rambler.ru>
2022-10-03 13:53:38 +0400

    faxed getIndex function

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const getRandomIndex = (data) => getRandomNumber(0, data.length);
+const getRandomIndex = (data) => getRandomNumber(0, data.length - 1);
 
 export {
   getRandomNumber,
